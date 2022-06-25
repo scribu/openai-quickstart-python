75df8051abde9023efb8709235a5a798facf1cbf
scribu <mail@scribu.net>
2022-06-25 11:33:46 +0100

    emphasis prompts

--- app.py
+++ app.py
@@ -6,30 +6,25 @@ from flask import Flask, redirect, render_template, request, url_for
 app = Flask(__name__)
 openai.api_key = os.getenv("OPENAI_API_KEY")
 
+TRAINING = """
+"YOU ate the potato" - it was you who ate it, rather than somebody else
+"You ate THE potato" - you ate the important potato, rather than some non-important potato"""
+
 
 @app.route("/", methods=("GET", "POST"))
 def index():
     if request.method == "POST":
-        animal = request.form["animal"]
+        prompt = TRAINING + "\n" + request.form["input"]
+
         response = openai.Completion.create(
             model="text-davinci-002",
-            prompt=generate_prompt(animal),
-            temperature=0.6,
+            prompt=prompt,
+            temperature=float(request.form["temperature"]),
         )
-        return redirect(url_for("index", result=response.choices[0].text))
+        result = response.choices[0].text
+    else:
+        result = ""
 
-    result = request.args.get("result")
-    return render_template("index.html", result=result)
-
-
-def generate_prompt(animal):
-    return """Suggest three names for an animal that is a superhero.
-
-Animal: Cat
-Names: Captain Sharpclaw, Agent Fluffball, The Incredible Feline
-Animal: Dog
-Names: Ruff the Protector, Wonder Canine, Sir Barks-a-Lot
-Animal: {}
-Names:""".format(
-        animal.capitalize()
-    )
+    input = request.form.get("input", "")
+    temperature = request.form.get("temperature", "0.6")
+    return render_template("index.html", result=result, input=input, temperature=temperature)
